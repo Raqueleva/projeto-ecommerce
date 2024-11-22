559cca1310a5277241ba5ae48208e46a3218a49c
Raqueleva <165317192+Raqueleva@users.noreply.github.com>
2024-11-22 11:59:51 -0300

    segundo commit

--- pedido/models.py
+++ pedido/models.py
@@ -6,6 +6,7 @@ class Pedido(models.Model):
     usuario = models.ForeignKey(User, on_delete=models.CASCADE)
     total = models.FloatField()
     qtd_total = models.PositiveIntegerField()
+    id = models.BigAutoField(primary_key=True)
     status = models.CharField(
         default="C",
         max_length=1,
